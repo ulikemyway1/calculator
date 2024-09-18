c8a3bee08870fd3e23c0910c07f3fcbdfac4ea54
ulikemywa1 <calium@outlook.com>
2024-09-18 19:38:33 +0300

    feat: implement helper function createDOMElement

---
+++ src/helpers/createDOMElement.js
@@ -0,0 +1,13 @@
+export default function createDOMElement({tag, classList, textContent}) {
+    const element = document.createElement(tag || 'div');
+
+    if (classList) {
+        element.classList.add(...classList);
+    }
+
+    if (textContent) {
+        element.textContent = textContent;
+    }
+
+    return element;
+}
