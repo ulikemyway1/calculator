fdef29f43a0e77ad4b2f78029479b6aa4e5d4fa9
ulikemywa1 <calium@outlook.com>
2024-09-18 17:39:52 +0300

    init: start developing

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2024: true,
+  },
+  extends: ['eslint:recommended', 'airbnb', 'prettier'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+    },
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js'],
+      },
+      'import/extensions': {
+        'import/extensions': 'never',
+      },
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Calculator',
+    }),
+    new ESLintPlugin(),
+  ],
+};
