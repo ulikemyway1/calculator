46854b3e89bceecae7144efa2102e881ef69d79e
ulikemywa1 <calium@outlook.com>
2024-09-21 15:33:33 +0300

    fix: fix invert symbol

--- src/components/computer/Computer.js
+++ src/components/computer/Computer.js
@@ -70,29 +70,37 @@ export default class Computer {
     this.expression = [];
   };
 
-  _pushCharInCalculatorMemory = (char) => this.expression.push(char);
+  _pushCharInCalculatorMemory = (char) => {
+    char
+      .toString()
+      .split('')
+      .forEach((symbol) => this.expression.push(symbol));
+  };
 
   _invertNumberSign = () => {
     const lastNumberChars = [];
-
+    let isMinusNow = false;
     let j = 1;
     let lastElement = this.expression[this.expression.length - j];
 
-    while (!Number.isNaN(Number(lastElement))) {
-      lastNumberChars.push(lastElement);
-      j += 1;
+    while (
+      this._charIsNumber(lastElement) ||
+      lastElement === textRepresentation.minus
+    ) {
+      if (!(isMinusNow && lastElement !== textRepresentation.minus))
+        lastNumberChars.push(lastElement);
 
+      isMinusNow = lastElement === textRepresentation.minus;
+      j += 1;
       lastElement = this.expression[this.expression.length - j];
     }
 
-    j = 1;
     lastElement = lastNumberChars.reverse().join('');
 
-    for (let i = 0; i < lastElement.length; i += 1) {
-      this._deleteLastCharFromExpression();
-    }
-    this._pushCharInCalculatorMemory(Number(lastElement) * -1);
-    this._recalculateWithNewChar(null);
+    this._deleteSomeLastChars(lastElement.length);
+
+    const invertedChar = Number(lastElement) * -1;
+    this._recalculateWithNewChar(invertedChar);
   };
 
   _displayCurrentExpression = (exp) => {
@@ -105,7 +113,9 @@ export default class Computer {
 
   _recalculateWithNewChar = (char) => {
     if (char) this._pushCharInCalculatorMemory(char);
+
     const fullExpression = this._normalizeExpression(this.expression.join(''));
+
     this._displayCurrentExpression(fullExpression);
     this._displayCurrentResult(fullExpression);
   };
@@ -117,7 +127,7 @@ export default class Computer {
 
   _isChangingOperatorProcess = (prevChar, newCharInfo) =>
     prevChar &&
-    Number.isNaN(Number(prevChar)) &&
+    !this._charIsNumber(prevChar) &&
     newCharInfo.role !== 'number' &&
     newCharInfo.role !== 'delete' &&
     newCharInfo.role !== 'evaluation' &&
@@ -127,4 +137,12 @@ export default class Computer {
     prevChar !== textRepresentation.delete &&
     prevChar !== textRepresentation.leftParenthesis &&
     prevChar !== textRepresentation.rightParenthesis;
+
+  _deleteSomeLastChars(n) {
+    for (let i = 0; i < n; i += 1) {
+      this._deleteLastCharFromExpression();
+    }
+  }
+
+  _charIsNumber = (char) => !Number.isNaN(Number(char));
 }
